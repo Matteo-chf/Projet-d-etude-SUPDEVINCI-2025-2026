7329d63c659a511b83f73e166b3b63b8615d4ea1
Matteo Cherief <matteo.cherief@orange.fr>
2026-02-26 16:56:18 +0100

    pipeline kedro

--- pipeline-kedro/src/pipeline_kedro/pipelines/nlp_cleaning/nodes.py
+++ pipeline-kedro/src/pipeline_kedro/pipelines/nlp_cleaning/nodes.py
@@ -1,4 +1,24 @@
 """
 This is a boilerplate pipeline 'nlp_cleaning'
 generated using Kedro 1.0.0
+#pour clean les données textuelles
 """
+import pandas as pd
+import re
+
+def uniformize_posts(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Uniformise les colonnes et le texte pour le modèle ML.
+    """
+    # 1. Nettoyage basique du texte
+    if "text" in df.columns:
+        df["text_cleaned"] = df["text"].astype(str).str.lower()
+        # Suppression des URLs
+        df["text_cleaned"] = df["text_cleaned"].apply(lambda x: re.sub(r'http\S+', '', x))
+    
+    # 2. Gestion du format de date
+    if "createdAt" in df.columns:
+        df["createdAt"] = pd.to_datetime(df["createdAt"])
+
+    # 3. Sélection des colonnes finales pour le catalogue
+    return df[["text_cleaned", "createdAt"]]
